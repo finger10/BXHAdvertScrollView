d5b4d07e292fe73a7eab8a030fb95dab9df2149c
guoguo <1686147171@qq.com>
2017-11-29 17:24:11 +0800

    Initial Commit

---
+++ BXHAdvertScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BXHAdvertScrollView
+//
+//  Created by gg on 2017/11/29.
+//  Copyright © 2017年 JayS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BXHAdvertScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BXHAdvertScrollView
+//
+//  Created by gg on 2017/11/29.
+//  Copyright © 2017年 JayS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
